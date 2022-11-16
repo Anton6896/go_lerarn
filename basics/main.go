a0d6362e1e5605fb2b701d4d65d5418d413c2a2f
Anton.R <Anton-R@live.com>
2022-11-16 21:53:28 +0200

    arays ok

--- basics/main.go
+++ basics/main.go
@@ -60,8 +60,38 @@ func great1() {
 	fmt.Printf("inpositionA %v \n", positionA&roleA == positionA)
 }
 
-// continue at 1.47
+func arrays() {
+	arr1 := [...]int{1, 2, 3} // ... add same as i put in initiation
+	for i := 0; i < len(arr1); i++ {
+		fmt.Println(arr1[i])
+	}
+
+	var arr2 [3]string
+	arr2[1] = "suprime"
+	fmt.Println(arr2[1])
+
+	var identityMatrix [3][3]int = [3][3]int{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}
+	for i := 0; i < len(identityMatrix); i++ {
+		fmt.Println(identityMatrix[i])
+	}
+
+	// array slice
+	fmt.Println(identityMatrix[0][1:])
+	fmt.Println(identityMatrix[1][:2])
+	fmt.Println(identityMatrix[0][:2])
+
+	// concat 2 slices
+	arr3 := []int{1, 2, 3}
+	arr4 := []int{4, 5, 6}
+	fmt.Println(arr3)
+	arr3 = append(arr3, arr4...)
+	fmt.Println(arr3)
+
+
+}
+
+// continue at 2.17
 func main() {
-	great1()
+	arrays()
 
 }
